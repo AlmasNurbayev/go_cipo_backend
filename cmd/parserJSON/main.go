5aa1cac71db60a294bf8e220f00d226a8087cd41
almas <ntldrzic@gmail.com>
2026-04-23 17:54:00 +0500

    feat: implement entry point to initialize and run the JSON parser

--- cmd/parserJSON/main.go
+++ cmd/parserJSON/main.go
@@ -1,1 +1,11 @@
 package parserjson
+
+import "github.com/AlmasNurbayev/go_cipo_backend/internal/parserJSON"
+
+func main() {
+	var Version = "v0.1.0"
+
+	p := parserJSON.New(Version)
+	p.Init()
+	p.Run()
+}
