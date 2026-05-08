129fc59a454cbce2c78ea77cc41c10d2c3455d4b
almas <ntldrzic@gmail.com>
2026-05-08 15:15:51 +0500

    refactor: remove Salt field from User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -11,7 +11,6 @@ type UserEntity struct {
 	Email        string      `json:"email" db:"email"`
 	Name         null.String `json:"name" db:"name"`
 	Password     null.String `json:"password" db:"password"`
-	Salt         null.String `json:"salt" db:"salt"`
 	Role         null.String `json:"role" db:"role"`
 	Changed_date time.Time   `json:"changed_date" db:"changed_date"`
 	Create_date  time.Time   `json:"create_date" db:"create_date"`
